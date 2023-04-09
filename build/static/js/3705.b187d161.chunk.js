8441bcd98971e31e9d85dcbb16ca1439a8d7f11c
varunsinghDG <85149973+varunsinghDG@users.noreply.github.com>
2023-04-09 16:30:49 +0530

    changes

---
+++ build/static/js/3705.b187d161.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_minimal_minimal_kit_react=self.webpackChunk_minimal_minimal_kit_react||[]).push([[3705],{33705:function(i,a,e){e.r(a);var m=e(43802);a.default=m.g}}]);
