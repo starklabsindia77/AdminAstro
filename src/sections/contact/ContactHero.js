2eff816c1e204628ebb17f7a2a4069b901d97e21
Varun Pratap Singh <starklabsindia@gmail.com>
2023-06-27 19:04:48 +0530

    changes

--- src/sections/contact/ContactHero.js
+++ src/sections/contact/ContactHero.js
@@ -11,7 +11,7 @@ const CONTACTS = [
   {
     country: 'India',
     address: '532, ICPL, Evergreen Towers, Sector 125, Mohali, Rupnagar, Punjab -140301',
-    phoneNumber: '+91 9816627678',
+    phoneNumber: '+91 7807344554',
   },
   // {
   //   country: 'London',
