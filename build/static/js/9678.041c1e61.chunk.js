a37e8b6efe3578472fe4c6154b66a3a32fab2a00
varun singh <pratap@arpiantech.com>
2024-07-07 20:51:51 +0530

    new build

---
+++ build/static/js/9678.041c1e61.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkastroscore=self.webpackChunkastroscore||[]).push([[9678],{19678:function(s,e,r){r.r(e);var a=r(32015);e.default=a.g}}]);
+//# sourceMappingURL=9678.041c1e61.chunk.js.map
