9a5d604c98446344d3e1f206ab55f0dc87019321
varun singh <pratap@arpiantech.com>
2024-07-07 20:45:29 +0530

    minor update

--- src/config.js
+++ src/config.js
@@ -9,8 +9,8 @@ import { PATH_DASHBOARD } from './routes/paths';
 // export const HOST_API = process.env.REACT_APP_HOST_API_KEY || '';
 // ----------------------------------------------------------------------export const HOST_API = "http://localhost:3001/api/admin"; 
 // export const HOST_API = "https://astroapi.onrender.com/api/admin"; 
-// export const HOST_API = "http://astroscore.ai:3001/api/admin"; 
-export const HOST_API = "http://localhost:3001/api/admin";
+export const HOST_API = "http://astroscore.ai:3001/api/admin"; 
+// export const HOST_API = "http://localhost:3001/api/admin";
 // export const HOST_API = "arn:aws:lambda:ap-south-1:847092605256:function:backend/api/admin";
 
 
